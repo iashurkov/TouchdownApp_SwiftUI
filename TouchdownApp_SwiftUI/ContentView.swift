5d61a7667523e49e558a4112b02b24143e97714b
iashurkov <iashurkov@uma.tech>
2022-12-26 16:33:40 +0300

    Initial Commit

---
+++ TouchdownApp_SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TouchdownApp_SwiftUI
+//
+//  Created by Igor Ashurkov on 26.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TouchdownApp_SwiftUI/TouchdownApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp_SwiftUIApp.swift
+//  TouchdownApp_SwiftUI
+//
+//  Created by Igor Ashurkov on 26.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
