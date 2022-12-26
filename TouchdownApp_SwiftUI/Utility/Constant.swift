eeba1887078c4fea22538addc0590a312deabdd0
iashurkov <iashurkov@uma.tech>
2022-12-26 17:42:21 +0300

    Settings project.

---
+++ TouchdownApp_SwiftUI/Utility/Constant.swift
@@ -0,0 +1,26 @@
+//
+//  Constant.swift
+//  TouchdownApp_SwiftUI
+//
+//  Created by Igor Ashurkov on 26.12.2022.
+//
+
+import SwiftUI
+
+enum Constant {
+    
+    // DATA
+    
+    // COLOR
+    
+    static let colorBackground = Color("ColorBackground")
+    static let colorGray = Color(UIColor.systemGray4)
+    
+    // LAYOUT
+    // UX
+    // API
+    // IMAGE
+    // FONT
+    // STRING
+    // MISC
+}
